09ccb65d9ad381d5f239c3907c01f5eec49153b5
pch8388 <pch838811@gmail.com>
2020-10-21 00:16:13 +0900

    create back-end scafoold

---
+++ src/test/java/study/taskagile/springvue/SpringVueApplicationTests.java
@@ -0,0 +1,13 @@
+package study.taskagile.springvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
