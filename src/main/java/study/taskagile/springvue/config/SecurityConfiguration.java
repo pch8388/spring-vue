c1eafa1af4d4ed64d08df221d72271b203f441eb
pch8388 <pch838811@gmail.com>
2020-10-26 15:54:21 +0900

    [feat] add spring security

---
+++ src/main/java/study/taskagile/springvue/config/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package study.taskagile.springvue.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private static final String[] PUBLIC = {
+        "/error", "/login", "/logout", "/register", "/api/registrations"
+    };
+
+    private static final String[] STATIC_RESOURCES = {
+        "/static/**", "/js/**", "/css/**", "/images/**", "/favicon.ico"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .and()
+            .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logged-out")
+                .and()
+            .csrf().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+        web.ignoring()
+            .antMatchers(STATIC_RESOURCES);
+    }
+}
