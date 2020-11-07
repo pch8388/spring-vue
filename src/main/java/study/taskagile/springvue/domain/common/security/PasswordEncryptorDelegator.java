db61bf7aa92f92028f7138424ad4aac7b71578e6
pch8388 <pch838811@gmail.com>
2020-11-07 22:09:26 +0900

    [feat] implement PasswordEncryptorDelegator

--- src/main/java/study/taskagile/springvue/domain/common/security/PasswordEncryptorDelegator.java
+++ src/main/java/study/taskagile/springvue/domain/common/security/PasswordEncryptorDelegator.java
@@ -1,12 +1,17 @@
 package study.taskagile.springvue.domain.common.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class PasswordEncryptorDelegator implements PasswordEncryptor {
+
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public String encrypt(String rawPassword) {
-        // TODO : 2020/10/25 구현필요   -ksc
-        return rawPassword;
+        return passwordEncoder.encode(rawPassword);
     }
 }
